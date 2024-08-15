eef5b0a83c02b55ee58e86f0e77c462ba69546e9
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-15 01:40:17 -0300

    Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,10 @@
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
+      <UserPhoto source={{ uri: 'https://github.com/arthurrios.png'}} w='$16' h='$16' alt='Imagem do usuário' />
       <VStack>
         <Text color="$gray100" fontSize="$sm">
           Olá
