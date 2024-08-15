ac50177acdf176ef8e5d95c058be131169f1357b
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-15 01:03:42 -0300

    Utilizando o Bottom Tab

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
-
-import { AuthRoutes } from './auth.routes'
-import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 import { Box } from '@gluestack-ui/themed'
+import { gluestackUIConfig } from '../../config/gluestack-ui.config'
+import { AppRoutes } from './app.routes'
 
 export function Routes() {
   const theme = DefaultTheme
@@ -11,7 +10,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
