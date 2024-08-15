423346a106bcecfc743856c2183c178de34b7673
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-15 01:58:31 -0300

    Exibindo o grupo selecionado

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,28 @@
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { Center } from '@gluestack-ui/themed'
+import { HStack, Center } from '@gluestack-ui/themed'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
-    <Center flex={1} >
+    <Center flex={1}>
       <HomeHeader />
+
+      <HStack>
+        <Group
+          name="Costas"
+          isActive={groupSelected === 'costas'}
+          onPress={() => setGroupSelected('costas')}
+        />
+
+        <Group
+          name="Ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </Center>
   )
 }
