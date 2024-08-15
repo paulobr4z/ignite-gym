e8aad8a35bfa5492656c25c08d51a11dc8322581
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-14 23:04:12 -0300

    Estilizando o focus no Input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,8 @@
-import { Image, VStack } from '@gluestack-ui/themed'
+import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
 
 import BackgroundImg from '@assets/background.png'
+import Logo from '@assets/logo.svg'
+import { Input } from '@components/Input'
 
 export function SignIn() {
   return (
@@ -13,6 +15,22 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
+
+          <Text color="$gray100" fontSize="$sm">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <Center gap="$2">
+          <Heading color="$gray100">Acesse sua conta</Heading>
+
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" />
+        </Center>
+      </VStack>
     </VStack>
   )
 }
