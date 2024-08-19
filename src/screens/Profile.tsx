ac96284939927dd733fea40c9befdc4d7acf23b5
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-19 01:22:56 -0300

    Inputs de nome e e-mails

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,3 +1,4 @@
+import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
 import { Center, Text, VStack } from '@gluestack-ui/themed'
@@ -14,6 +15,7 @@ export function Profile() {
             size="xl"
             alt="Imagem do usuário"
           />
+
           <TouchableOpacity>
             <Text
               color="$green500"
@@ -25,6 +27,11 @@ export function Profile() {
               Alterar Foto
             </Text>
           </TouchableOpacity>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input value="paulo@email.com" bg="$gray600" isReadOnly />
+          </Center>
         </Center>
       </ScrollView>
     </VStack>
