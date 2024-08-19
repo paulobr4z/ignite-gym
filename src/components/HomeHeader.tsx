4ac8bf0473831f4d6dc9b4bdd282fc7adefc6718
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-19 00:48:36 -0300

    Header do Perfil do Usuário

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -4,20 +4,24 @@ import { LogOut } from 'lucide-react-native'
 
 export function HomeHeader() {
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
-      <UserPhoto source={{ uri: 'https://github.com/arthurrios.png'}} w='$16' h='$16' alt='Imagem do usuário' />
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: 'https://github.com/paulobr4z.png' }}
+        w="$16"
+        h="$16"
+        alt="Imagem do usuário"
+      />
 
       <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">
           Olá
         </Text>
         <Heading color="$gray100" fontSize="$md">
-          Arthur Rios
+          Paulo Braz
         </Heading>
       </VStack>
 
-      <Icon as={LogOut} color='$gray200' size='xl' />
-
+      <Icon as={LogOut} color="$gray200" size="xl" />
     </HStack>
   )
 }
