cfeac2c6ee8c5e58dd806c9f9c0c12a1a5ff49d4
paulobr4z <paulobrazaraujo@gmail.com>
2024-09-16 20:42:33 -0300

    Galeria de Fotos: Recuperando foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -8,12 +8,18 @@ import * as ImagePicker from 'expo-image-picker'
 
 export function Profile() {
   async function handleUserPhotoSelect() {
-    await ImagePicker.launchImageLibraryAsync({
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
       aspect: [4, 4],
       allowsEditing: true,
     })
+
+    if (photoSelected.canceled) {
+      return
+    }
+
+    console.log(photoSelected.assets[0])
   }
 
   return (
