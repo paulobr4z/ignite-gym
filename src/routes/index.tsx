717f53b8017fd81bd02bab7034645d7a6608b2b3
paulobr4z <paulobrazaraujo@gmail.com>
2024-09-17 20:16:43 -0300

    Contextos no React Native - Criando seu próprio Hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,8 @@
-import { useContext } from 'react'
-
 import { Box } from '@gluestack-ui/themed'
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
-import { AuthContext } from '@contexts/AuthContext'
+import { useAuth } from '@hooks/useAuth'
 
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
@@ -13,9 +11,9 @@ export function Routes() {
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
-  const contextData = useContext(AuthContext)
+  const { user } = useAuth()
 
-  console.log('USUÁRIO LOGADO =>', contextData)
+  console.log('USUÁRIO LOGADO =>', user)
 
   return (
     <Box flex={1} bg="$gray700">
