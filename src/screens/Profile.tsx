03ec84cf37566632efe16a1fef456603be7e884f
paulobr4z <paulobrazaraujo@gmail.com>
2024-09-16 22:23:10 -0300

    Galeria de Fotos: Criar o componente de Toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -7,6 +7,7 @@ import { Center, Heading, Text, VStack } from '@gluestack-ui/themed'
 import { Alert, ScrollView, TouchableOpacity } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as FileSystem from 'expo-file-system'
+import { ToastMessage } from '@components/ToastMessage'
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState('https://github.com/paulobr4z.png')
@@ -47,6 +48,13 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+      <ToastMessage
+        id="1"
+        title="Mensagem de exemplo"
+        description="asdasdakjsd asdajksdbasjdhasd"
+        action="success"
+        onClose={() => {}}
+      />
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
