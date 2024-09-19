2d6ff3f8b4893b2835d3415fed3dff940bf173f8
paulobr4z <paulobrazaraujo@gmail.com>
2024-09-19 18:43:37 -0300

    Autenticação JWT - Removendo o token do storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,8 +3,9 @@ import { createContext, ReactNode, useEffect, useState } from 'react'
 import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
 import {
-  storageAuthTokenGet,
   storageAuthTokenSave,
+  storageAuthTokenGet,
+  storageAuthTokenRemove,
 } from '@storage/storageAuthToken'
 import {
   storageUserGet,
@@ -91,6 +92,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true)
       setUser({} as UserDTO)
       await storageUserRemove()
+      await storageAuthTokenRemove()
     } catch (error) {
       throw error
     } finally {
